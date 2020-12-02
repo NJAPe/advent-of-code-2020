b2ed37f2ca23866e628d1dbf4528133d2fe5d9d0
Joel Pettersson <joel.pettersson@hypergene.se>
2020-12-02 19:15:21 +0100

    minor cleanup

--- src/02.py
+++ src/02.py
@@ -6,40 +6,36 @@ def parse_row(row):
     rule = [int(part) for part in rule_raw.split('-')]
     return rule, letter.strip(':'), pwd
 
-def is_valid(rule, letter, pwd):
+def is_valid_part_1(rule, letter, pwd):
     num_occ = pwd.count(letter)
     return rule[0] <= num_occ and num_occ <= rule[1]
 
 def is_valid_part_2(rule, letter, pwd):
     return (pwd[rule[0] - 1] == letter) ^ (pwd[rule[1] - 1] == letter)
 
-def solve(inp, part_2=False):
+def solve(inp, validator):
     count = 0
     for row in inp:
         row_parts = parse_row(row)
-        if part_2:
-            if is_valid_part_2(*row_parts):
-                count += 1
-        else:
-            if is_valid(*row_parts):
-                count += 1
+        if validator(*row_parts):
+            count += 1
     return count
 
 def solve_part_1_test():
     inp = get_input('input\\02_test.txt')
-    print(f'Part 1 test: {solve(inp)}')
+    print(f'Part 1 test: {solve(inp, is_valid_part_1)}')
 
 def solve_part_1_real():
     inp = get_input('input\\02.txt')
-    print(f'Part 1: {solve(inp)}')
+    print(f'Part 1: {solve(inp, is_valid_part_1)}')
 
 def solve_part_2_test():
     inp = get_input('input\\02_test.txt')
-    print(f'Part 1 test: {solve(inp, part_2=True)}')
+    print(f'Part 1 test: {solve(inp, is_valid_part_2)}')
 
 def solve_part_2_real():
     inp = get_input('input\\02.txt')
-    print(f'Part 1: {solve(inp, part_2=True)}')
+    print(f'Part 1: {solve(inp, is_valid_part_2)}')
 
 
 
