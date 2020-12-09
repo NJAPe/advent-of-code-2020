0cded1c486fcae19be412c32da8eb93405e5e55a
Joel Pettersson <joel.pettersson@hypergene.se>
2020-12-09 12:36:20 +0100

    Day 9 part 2

--- src/09.py
+++ src/09.py
@@ -1,6 +1,9 @@
 from common import get_input
 
 
+def parse_input(file_name):
+    return [int(i) for i in get_input(file_name)]
+
 def has_sum(numbers, target):
     numbers.sort()
     start, stop = 0, len(numbers) - 1
@@ -15,7 +18,6 @@ def has_sum(numbers, target):
     return False
 
 def get_first_missing(inp, preamble):
-    inp = [int(i) for i in inp]
     idx = preamble
     while idx < len(inp):
         if has_sum(inp[idx - preamble:idx], inp[idx]):
@@ -23,10 +25,37 @@ def get_first_missing(inp, preamble):
         else:
             return inp[idx]
 
+
+def get_contiguos_sum_to(numbers, target):
+    tail, head = 0, 1
+    contiguos = [numbers[tail]]
+    while True:
+        contiguos.append(numbers[head])
+        curr_sum = sum(contiguos)
+        if curr_sum == target:
+            return contiguos
+        elif  curr_sum < target:
+            head += 1
+            if head == len(numbers):
+                raise ValueError('Not possible to make sum')
+        else:
+            tail += 1
+            head = tail + 1
+            contiguos = [numbers[tail]]
+    return None
+
 def solve_part_1(file_name, preamble):
-    missing = get_first_missing(get_input(file_name), preamble)
+    missing = get_first_missing(parse_input(file_name), preamble)
     print(f'Part 1 {file_name}: {missing}')
+
+def solve_part_2(file_name, preamble):
+    inp = parse_input(file_name)
+    missing = get_first_missing(inp, preamble)
+    contiguos = sorted(get_contiguos_sum_to(inp, missing))
+    print(f'Part 2 {file_name}: {contiguos[0] + contiguos[-1]}')
 
 if __name__ == "__main__":
     solve_part_1('input\\09_test.txt', 5)
     solve_part_1('input\\09.txt', 25)
+    solve_part_2('input\\09_test.txt', 5)
+    solve_part_2('input\\09.txt', 25)
