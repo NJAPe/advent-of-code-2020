c82c6c9c1b3f81da06c287caab9cfebb9db3da69
Joel Pettersson <joel.pettersson@hypergene.se>
2020-12-12 12:20:13 +0100

    Add simple time function decorator

--- src/common.py
+++ src/common.py
@@ -1,3 +1,5 @@
+import time
+
 def get_input(filename):
     with open(filename) as _f:
         return _f.read().split('\n')
@@ -6,3 +8,14 @@ def get_input(filename):
 def get_int_input(filename):
     with open(filename) as _f:
         return [int(line) for line in _f]
+
+def time_function(function):
+    def wrap(*args, **kwargs):
+        print('\nTiming execution:')
+        start = time.time()
+        ret = function(*args, **kwargs)
+        stop = time.time()
+        print(f'Execution took {stop - start}s\n')
+        return ret
+    return wrap
+
