8d2bcc9d6977145971f708cdaa2c4d5f1d4cee23
Joel Pettersson <joel.pettersson@hypergene.se>
2020-12-14 18:51:24 +0100

    Add license note for chinese reminder

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,6 +1,16 @@
+# Implementation for finding the common divisor (chinese_remainder, mul_inv)
+# is from Rosetta Code:
+#   https://rosettacode.org/wiki/Chinese_remainder_theorem#Python
+# The contents of this file are released under the GNU FDL 1.2 License.
+# Permission is granted to copy, distribute and/or modify this document
+# under the terms of the GNU Free Documentation License, Version 1.2
+# or any later version published by the Free Software Foundation;
+# with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+# Texts.  A copy of the license is available at the following URL:
+#   https://www.gnu.org/licenses/old-licenses/fdl-1.2.html
 from functools import reduce
 
-# Source https://rosettacode.org/wiki/Chinese_remainder_theorem#Python
+
 def chinese_remainder(n, a):
     sum = 0
     prod = reduce(lambda a, b: a*b, n)
